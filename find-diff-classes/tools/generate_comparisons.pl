949945606cd5e825e7ab414374a88ff13f7837f4
Tim White <walton.timothy.james.white@gmail.com>
2024-10-22 12:14:00 +1300

    Add --generate-tests and other options to generate Evosuite tests on mvnc jars

--- find-diff-classes/tools/generate_comparisons.pl
+++ find-diff-classes/tools/generate_comparisons.pl
@@ -6,6 +6,7 @@ use warnings;
 my $BASE = "/home/wtwhite/code/craw-redhat-oss/wget/crawl";
 my $CLASSPATH = "/home/wtwhite/code/tooling/target/bineq-1.0.0-jar-with-dependencies.jar";
 my $COMPAREJARS = "java -cp $CLASSPATH io.github.bineq.cli.CompareJars";
+my $EVOSUITEJAR = "$ENV{HOME}/code/regression-test-generation/evosuite/evosuite-1.2.0.jar";
 
 sub gavToPath($$$) {
 	$_[0] =~ s|\.|/|g;
@@ -56,19 +57,112 @@ sub providerPath($$$$) {
 	die "Could not find matching dir for provider $_[0] for $g:$a:$v!";
 }
 
-while (<>) {
-	chomp;
-	my ($g, $a, $v, $p1, $p2) = split /\t/ or die;
-	my $jarPath = gavToJarPath($g, $a, $v);
-	#my $jarPath1 = providerPath($p1) . "/" . $jarPath;
-	#my $jarPath2 = providerPath($p2) . "/" . $jarPath;
-	#my $jarPath1 = providerPath($p1, $jarPath);
-	#my $jarPath2 = providerPath($p2, $jarPath);
-	my $jarPath1 = providerPath($p1, $g, $a, $v);
-	my $jarPath2 = providerPath($p2, $g, $a, $v);
-	my $outDir = "results/$jarPath.compare";
-	my $mkdirCmd = "mkdir -p $outDir";
+sub evosuiteGenDir($$$$) {
+	my ($p, $g, $a, $v) = @_;
+	#my $providerPath = providerPath($p, $g, $a, $v);
+	return join("/", "testgen", $p, $g =~ s|\.|/|gr, $a, $v);
+}
 
-	my $cmd = "$mkdirCmd && $COMPAREJARS -eq jnorm2,javap -j1 $jarPath1 -j2 $jarPath2 -out $outDir/$p1.vs.$p2.json";
-	print $cmd, "\n";
+sub generateComparisons() {
+	while (<>) {
+		chomp;
+		my ($g, $a, $v, $p1, $p2) = split /\t/ or die;
+		my $jarPath = gavToJarPath($g, $a, $v);
+		#my $jarPath1 = providerPath($p1) . "/" . $jarPath;
+		#my $jarPath2 = providerPath($p2) . "/" . $jarPath;
+		#my $jarPath1 = providerPath($p1, $jarPath);
+		#my $jarPath2 = providerPath($p2, $jarPath);
+		my $jarPath1 = providerPath($p1, $g, $a, $v);
+		my $jarPath2 = providerPath($p2, $g, $a, $v);
+		my $outDir = "results/$jarPath.compare";
+		my $mkdirCmd = "mkdir -p $outDir";
+
+		my $cmd = "$mkdirCmd && $COMPAREJARS -eq jnorm2,javap -j1 $jarPath1 -j2 $jarPath2 -out $outDir/$p1.vs.$p2.json";
+		print $cmd, "\n";
+	}
+}
+
+sub generateOrRunTests($$) {
+	my ($shouldGenerateTests, $shouldRunTests) = @_;
+
+	my (@classesFiltered) = `find results -name 'classes.filtered'`;
+	chomp @classesFiltered;
+	#print scalar @classesFiltered;
+
+	my %providersForJar;
+	my %jarPathHasMvnc;
+	my %interestingClasses;
+	foreach my $fn (@classesFiltered) {
+		my ($jarPath, $p1, $p2) = $fn =~ m|^results/(.*)\.compare/(\w+)\.vs\.(\w+)/classes.filtered$| or die;
+		#my ($g, $a, $v) = $jarPath =~ m|^(.*)/([^/]+)/([^/]+)/[^/]+\.jar$| or die;
+		#$g =~ tr|/|.|;
+		push @{$providersForJar{$jarPath}}, [$p1, $p2];
+		if ($p1 eq 'mvnc' || $p2 eq 'mvnc') {
+			$jarPathHasMvnc{$jarPath} = 1;
+			my @classes = `cat $fn`;	# Ugh
+			chomp @classes;
+			push @{$interestingClasses{$jarPath}}, @classes;
+		}
+	}
+
+	foreach my $jarPath (sort keys %providersForJar) {
+		my ($g, $a, $v) = $jarPath =~ m|^(.*)/([^/]+)/([^/]+)/[^/]+\.jar$| or die;
+		#$g =~ tr|/|.|;
+		if ($jarPathHasMvnc{$jarPath}) {
+			if ($shouldGenerateTests) {
+				# Just build tests on Maven Central for now
+				my $p = 'mvnc';
+				#print join("\t", $jarPath, $p1, $p2), "\n";		#DEBUG
+				#print join("\t", providerPath($p, $g, $a, $v), $p1, $p2), "\n";		#DEBUG
+				my @classes = @{$interestingClasses{$jarPath}};
+				print "# Generate tests on the " . scalar(@classes) . " classes in the $p version of $jarPath\n";		#TODO
+
+				# Copy all dependencies just once for the project
+				my $testGenPath = evosuiteGenDir($p, $g, $a, $v);
+				my $pomPath = providerPath($p, $g, $a, $v) =~ s/\.jar$/.pom/r;
+				my $basePath = $pomPath =~ s|/[^/]+$||r;
+				my $mvnCopyDepsCmd = "( mkdir -p '$testGenPath' && cd '$testGenPath' && mvn -f $pomPath dependency:copy-dependencies && ln -s $basePath/target t";	# Symlink to make classpath shorter
+				print $mvnCopyDepsCmd, "\n";
+				my $classOwnCP = providerPath($p, $g, $a, $v);
+				#my $projectCP = "$classOwnCP:\$(echo target/dependency/*|perl -lpe 's/ /:/g')";
+				my $projectCP = "$classOwnCP:\$(echo t/dependency/*|perl -lpe 's/ /:/g')";
+
+				# Generate tests for each class
+				foreach my $class (@classes) {
+					my $dottedClass = $class =~ s|/|.|gr;
+					$dottedClass =~ s/\.class$// or die;
+					my $evosuiteGenTestsCmd = "time java -jar $EVOSUITEJAR -class $dottedClass -projectCP $projectCP 2>&1 | tee evosuite.$dottedClass.log";
+					print $evosuiteGenTestsCmd, "\n";
+				}
+
+				print ")\n";		# Drops us back to original subdir
+			}
+
+			if ($shouldRunTests) {
+				#TODO: Do per-provider-pair work
+				foreach my $providerPair (@{$providersForJar{$jarPath}}) {
+					my ($p1, $p2) = @{$providerPair};
+					print "# Compare $p1 to $p2 on $jarPath\n";
+				}
+			}
+		} else {
+			warn "Ignoring $jarPath, which doesn't have any provider pair involving mvnc.\n";
+		}
+	}
+}
+
+# Main program
+my $mode = shift;
+if (!defined $mode) {
+	die "Must specify a mode.";
+} elsif ($mode eq '--generate-comparisons') {
+	generateComparisons();
+} elsif ($mode eq '--generate-tests') {
+	generateOrRunTests(1, 0);
+} elsif ($mode eq '--run-tests') {
+	generateOrRunTests(0, 1);
+} elsif ($mode eq '--generate-and-run-tests') {
+	generateOrRunTests(1, 1);
+} else {
+	die "Unrecognised option '$mode'";
 }
