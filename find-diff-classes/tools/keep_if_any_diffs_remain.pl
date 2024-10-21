f4fa7f4770a10c5addb51b7168f9fbb0e511e00c
Tim White <walton.timothy.james.white@gmail.com>
2024-10-21 21:59:43 +1300

    Add tool to produce classes.filtered files for each jar pair

---
+++ find-diff-classes/tools/keep_if_any_diffs_remain.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $FILTERCMD = 'tools/filter_diff.pl';
+my $prevInFName;
+
+print <<THE_END;
+#!/bin/bash
+set -evo pipefail    # Echo input and exit at first error
+THE_END
+
+while (<>) {
+	my $inFName = $ARGV;
+	my $baseDir = $inFName;
+	$baseDir =~ s/\.tsv$// or die;
+	my $outFName = "$baseDir/classes.filtered";
+
+	if (!defined($prevInFName) || $prevInFName ne $inFName) {
+		print "# $inFName\n";
+
+		my $mkdirCmd = "mkdir -p '$baseDir'";
+		print $mkdirCmd, "\n";
+
+		my $cleanCmd = "rm -f '$outFName'";
+		print $cleanCmd, "\n";
+	}
+	$prevInFName = $inFName;
+
+	chomp;
+	my ($classSlashed, $jimpleFName1, $jimpleFName2) = split /\t/;
+	my $class = $classSlashed;
+	$class =~ s/\.class$// or die;
+	$class =~ tr|/|.|;
+	my $basename = "$baseDir/$class";
+
+	my $diffCmd = "diff '$jimpleFName1' '$jimpleFName2' > '$basename.diff' || true";		# "|| true" to ignore diff's nonzero exit code under set -e
+	print $diffCmd, "\n";
+
+	my $filterCmd = "$FILTERCMD < '$basename.diff' > '$basename.diff.filtered'";
+	print $filterCmd, "\n";
+
+	#my $ifCmd = "if [ -s '$basename.diff.filtered' ]; then echo '$classSlashed' >> '$outFName'; fi";
+	my $ifCmd = "if [ -s '$basename.diff.filtered' ]; then echo '$classSlashed' >> '$outFName'; fi";
+	print $ifCmd, "\n";
+}
