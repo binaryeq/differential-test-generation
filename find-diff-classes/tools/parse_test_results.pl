230f7d80d461ccf86ca4158e9deaf3375d79e702
Tim White <walton.timothy.james.white@gmail.com>
2024-10-25 14:48:58 +1300

    Add --include-failed-test-numbers option to output sequence numbers of failed tests in sorted order

--- find-diff-classes/tools/parse_test_results.pl
+++ find-diff-classes/tools/parse_test_results.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+my $shouldIncludeFailedTestNumbers = 0;
+
 sub slurp($) {
 	return scalar(`cat '$_[0]'`);	# Yuck
 }
@@ -16,9 +18,11 @@ sub process($) {
     my @failures;
     if (s/\AJUnit version 4\.13\.2\n([\.E]*)\nTime: ([^\n]+)\n//) {
         if (s/\AThere were (\d+) failures:\n//) {
-            while (s/\A\d+\) (\w+\(.*\))\n([^\t\n][^\n]*\n(?:\t[^\n]*\n)*)//) {
+            while (s/\A\d+\) test(\d+)\(.*\)\n([^\t\n][^\n]*\n(?:\t[^\n]*\n)*)//) {
                 push @failures, [ $1, $2 ];
             }
+
+            @failures = sort { $a <=> $b } @failures;
 
             if (s/\A\nFAILURES!!!\nTests run: (\d+),\s*Failures: (\d+)\n\n\z//) {
                 die "JUnit listed " . scalar(@failures) . " failures but then reported that $2 failed on the last line." if $2 != @failures;    # Sanity check
@@ -26,7 +30,6 @@ sub process($) {
             } else {
                 die "Could not parse: <$_>";
             }
-
         } elsif (s/\A\nOK \((\d+) tests?\)\n\n\z//) {
             $result = { "tests" => $1, "successes" => $1, "failures" => [] };
         } else {
@@ -34,9 +37,16 @@ sub process($) {
         }
     }
 
-    # TODO: Output the individual failures somehow, not just their count
-    print join("\t", $fn, @{$result}{"tests", "successes"}, scalar(@{$result->{"failures"}})), "\n";
+    # TODO: Output the individual failure details somehow, not just their count and their test sequence numbers
+    my @columns = ($fn, @{$result}{"tests", "successes"}, scalar(@{$result->{"failures"}}));
+    push @columns, map { $_->[0] } @{$result->{"failures"}} if $shouldIncludeFailedTestNumbers;
+    print join("\t", @columns), "\n";
 }
 
 # Main
+if (@ARGV && $ARGV[0] eq '--include-failed-test-numbers') {
+    $shouldIncludeFailedTestNumbers = 1;
+    shift;
+}
+
 process($_) foreach @ARGV;
