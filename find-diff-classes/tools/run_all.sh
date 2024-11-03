ab576b6b2b82ef6c7bacbc70755bdc7894b8e058
Tim White <walton.timothy.james.white@gmail.com>
2024-11-04 10:43:42 +1300

    Rename results/ to comparisons/

--- find-diff-classes/tools/run_all.sh
+++ find-diff-classes/tools/run_all.sh
@@ -17,19 +17,19 @@ tools/generate.pl --generate-comparisons < jnorm2_distinct_gavs.tsv > compare.sh
 #TODO: Fix this
 [ -e tools/jnorm-jar-with-dependencies.jar ] || curl -L -o tools/jnorm-jar-with-dependencies.jar https://github.com/stschott/jnorm-tool/releases/download/v1.0.0/jnorm-cli-1.0.0.jar
 
-# Run the script. Creates a results/ subdirectory hierarchy with a <provider1>.vs.<provider2>.json file per
+# Run the script. Creates a comparisons/ subdirectory hierarchy with a <provider1>.vs.<provider2>.json file per
 # (GAV, provider pair), listing the specific classes that differ in that GAV between those providers.
 time bash compare.sh
 
 # Create a shell script to convert the information about different individual classes from JSON to TSV.
 #TODO: Move this work inside the compare.sh run in the previous step
-tools/json_to_tsv.pl $(find results -name '*.vs.*.json') > convert_json_to_tsv.sh
+tools/json_to_tsv.pl $(find comparisons -name '*.vs.*.json') > convert_json_to_tsv.sh
 
 # Run the script.
 bash convert_json_to_tsv.sh
 
 # Create a shell script to filter out all "uninteresting" differences (e.g., invokeinterface <-> invokevirtual).
-tools/keep_if_any_diffs_remain.pl $(find results -name '*.vs.*.tsv') > filter_all_diffs.sh
+tools/keep_if_any_diffs_remain.pl $(find comparisons -name '*.vs.*.tsv') > filter_all_diffs.sh
 
 # Run the script.
 bash filter_all_diffs.sh
