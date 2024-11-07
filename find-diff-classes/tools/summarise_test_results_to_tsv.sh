73b574b103676b75f0219de06e7d268f9a11ce03
Tim White <walton.timothy.james.white@gmail.com>
2024-11-07 17:30:16 +1300

    Sort TSV results for easier diffing

--- find-diff-classes/tools/summarise_test_results_to_tsv.sh
+++ find-diff-classes/tools/summarise_test_results_to_tsv.sh
@@ -8,4 +8,4 @@ fi
 provider="$1"
 
 for f in `find run/$provider -name '*.out'`; do echo $f; tools/parse_test_results.pl --include-failed-test-numbers $f > $f.tsv || mv $f.tsv $f.tsv.failed; done
-cat `find run/$provider -name '*.out.tsv'` > test_results_$provider.tsv
+cat `find run/$provider -name '*.out.tsv'` | sort > test_results_$provider.tsv
