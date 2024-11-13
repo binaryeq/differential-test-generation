d86d4b331adf95f89f2ddb9aaac4c70e64c94efe
Tim White <walton.timothy.james.white@gmail.com>
2024-11-13 17:16:36 +1300

    Minimal

--- find-diff-classes/repro_flip_failure/Repro.java
+++ find-diff-classes/repro_flip_failure/Repro.java
@@ -1,23 +1,16 @@
-import io.undertow.connector.ByteBufferPool;
-import io.undertow.server.DefaultByteBufferPool;
 import io.undertow.server.XnioByteBufferPool;
 import io.undertow.servlet.spec.UpgradeServletInputStream;
-import io.undertow.testutils.DebuggingSlicePool;
 import java.nio.ByteBuffer;
-import java.util.concurrent.Executor;
-import javax.servlet.ReadListener;
 import org.xnio.Pool;
-import org.xnio.XnioIoThread;
 import org.xnio.channels.EmptyStreamSourceChannel;
-import org.xnio.channels.StreamSourceChannel;
 
 public class Repro {
   public static void main(String[] args)  throws Throwable  {
-      EmptyStreamSourceChannel emptyStreamSourceChannel0 = new EmptyStreamSourceChannel((XnioIoThread) null);
+      EmptyStreamSourceChannel emptyStreamSourceChannel0 = new EmptyStreamSourceChannel(null);
       Pool<ByteBuffer> pool0 = Pool.HEAP;
       XnioByteBufferPool xnioByteBufferPool0 = new XnioByteBufferPool(pool0);
-      UpgradeServletInputStream upgradeServletInputStream0 = new UpgradeServletInputStream(emptyStreamSourceChannel0, xnioByteBufferPool0, (Executor) null);
-      byte[] byteArray0 = new byte[7];
-      upgradeServletInputStream0.read(byteArray0);
+      UpgradeServletInputStream upgradeServletInputStream0 = new UpgradeServletInputStream(emptyStreamSourceChannel0, xnioByteBufferPool0, null);
+      byte[] byteArray0 = new byte[1];
+      upgradeServletInputStream0.read(byteArray0);    // throws NoSuchMethodError when running on JDK 8 but not JDK 11
   }
 }
