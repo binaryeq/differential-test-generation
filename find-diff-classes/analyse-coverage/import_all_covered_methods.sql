c09abb6cd7efa7c130a62e52aa71702e38904410
Tim White <walton.timothy.james.white@gmail.com>
2024-11-15 15:54:14 +1300

    Rename column test -> testClass for parity with test_classes

--- find-diff-classes/analyse-coverage/import_all_covered_methods.sql
+++ find-diff-classes/analyse-coverage/import_all_covered_methods.sql
@@ -1,6 +1,6 @@
 DROP INDEX IF EXISTS covered_methods_idx;
 DROP TABLE IF EXISTS covered_methods;
-CREATE TABLE covered_methods (provider TEXT, groupId TEXT, artifactId TEXT, version TEXT, test TEXT, package TEXT, class TEXT, method TEXT, descriptor TEXT);
+CREATE TABLE covered_methods (provider TEXT, groupId TEXT, artifactId TEXT, version TEXT, testClass TEXT, package TEXT, class TEXT, method TEXT, descriptor TEXT);
 .mode tabs
 .import all_covered_methods.tsv covered_methods
-CREATE UNIQUE INDEX covered_methods_idx on covered_methods (provider, groupId, artifactId, version, test, package, class, method, descriptor);
+CREATE UNIQUE INDEX covered_methods_idx on covered_methods (provider, groupId, artifactId, version, testClass, package, class, method, descriptor);
