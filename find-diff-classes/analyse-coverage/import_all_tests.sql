6b0eb5c2e4ab5eb1bd8be3964759721f8973bbb0
Tim White <walton.timothy.james.white@gmail.com>
2024-11-15 15:58:04 +1300

    Add jacoco_ prefix to both Jacoco-based DB table names

--- find-diff-classes/analyse-coverage/import_all_tests.sql
+++ find-diff-classes/analyse-coverage/import_all_tests.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS tests;
-CREATE TABLE tests (provider TEXT, groupId TEXT, artifactId TEXT, version TEXT, testClass TEXT);
+DROP TABLE IF EXISTS jacoco_tests;
+CREATE TABLE jacoco_tests (provider TEXT, groupId TEXT, artifactId TEXT, version TEXT, testClass TEXT);
 .mode tabs
-.import all_tests.tsv tests
+.import all_tests.tsv jacoco_tests
