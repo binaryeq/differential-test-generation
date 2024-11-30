4253b08af2f07be91783072da97267b5a20bc19d
Tim White <walton.timothy.james.white@gmail.com>
2024-11-30 17:06:18 +1300

    Get rid of noise on stdout

--- find-diff-classes/tools/summarise_test_results_to_tsv.sh
+++ find-diff-classes/tools/summarise_test_results_to_tsv.sh
@@ -9,5 +9,5 @@ fi
 
 provider="$1"
 
-for f in `find run/$provider -name '*.out'`; do echo $f; tools/parse_test_results.pl --include-failed-test-numbers $f > $f.tsv || mv $f.tsv $f.tsv.failed; done
+for f in `find run/$provider -name '*.out'`; do echo $f >&2; tools/parse_test_results.pl --include-failed-test-numbers $f > $f.tsv || mv $f.tsv $f.tsv.failed; done
 cat `find run/$provider -name '*.out.tsv'` | sort
