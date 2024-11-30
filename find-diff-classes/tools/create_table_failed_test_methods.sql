baa37488b89c50a663060a01942631028776c151
Tim White <walton.timothy.james.white@gmail.com>
2024-12-01 03:02:55 +1300

    Add separate clean-db target for better DB handling

--- find-diff-classes/tools/create_table_failed_test_methods.sql
+++ find-diff-classes/tools/create_table_failed_test_methods.sql
@@ -1,2 +1,1 @@
-DROP TABLE IF EXISTS failed_test_methods;
 CREATE TABLE failed_test_methods (provider TEXT, groupId TEXT, artifactId TEXT, version TEXT, testClass TEXT, testMethodSuffix TEXT);
--- find-diff-classes/tools/create_table_test_classes.sql
+++ find-diff-classes/tools/create_table_test_classes.sql
@@ -1,2 +1,1 @@
-DROP TABLE IF EXISTS test_classes;
 CREATE TABLE test_classes (provider TEXT, groupId TEXT, artifactId TEXT, version TEXT, testClass TEXT, nTests INTEGER, nTestsSucceeded INTEGER, nTestsFailed INTEGER, elapsedTime REAL);
